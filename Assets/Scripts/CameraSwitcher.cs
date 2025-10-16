d0735ddaf08cf84c9bab7069e6a0a9ace3d354b3
TGSR <81268811+TGSR-code@users.noreply.github.com>
2025-10-16 12:40:25 +0200

    menu updated, CameraSwitcher Movement fixed

--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -1,79 +1,45 @@
 using UnityEngine;
 
-public class CameraSwitcher : MonoBehaviour
+public class CameraHandler : MonoBehaviour
 {
-    [Header("Cameras")]
+
+
+    [Header("Camera")]
     [SerializeField] Camera MainCamera;
     [SerializeField] Camera TopViewCamera;
 
     [Header("Settings")]
     [SerializeField] KeyCode SwitchKey = KeyCode.Tab;
     [SerializeField] KeyCode ResetKey = KeyCode.R;
-    [SerializeField] float TopViewSpeed = 8f;
+    [SerializeField] int TopViewSpeed = 8;
     [SerializeField] Transform ResetPoint;
 
-    [SerializeField] PlayerMovement playerMovement;
-
-    bool mainActive = true;
-    Rigidbody topRb;
-    Rigidbody playerRb;
-    Vector3 topInput;
+    bool MainActive = true;
 
     void Start()
     {
-        MainCamera?.gameObject.SetActive(true);
-        TopViewCamera?.gameObject.SetActive(false);
-
-        if (playerMovement == null)
-            playerMovement = FindAnyObjectByType<PlayerMovement>();
-
-        playerRb = playerMovement?.GetComponent<Rigidbody>();
-
-        if (TopViewCamera != null)
-        {
-            topRb = TopViewCamera.GetComponent<Rigidbody>() ?? TopViewCamera.gameObject.AddComponent<Rigidbody>();
-            topRb.useGravity = false;
-            topRb.constraints = RigidbodyConstraints.FreezeRotation;
-            topRb.interpolation = RigidbodyInterpolation.Interpolate;
-            topRb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
-        }
+        MainCamera.gameObject.SetActive(true);
+        TopViewCamera.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(SwitchKey))
+        if (Input.GetKeyDown(SwitchKey)) //true false switch
         {
-            mainActive = !mainActive;
-            MainCamera?.gameObject.SetActive(mainActive);
-            TopViewCamera?.gameObject.SetActive(!mainActive);
-
-            if (playerMovement != null)
-            {
-                playerMovement.enabled = mainActive;
-                if (!mainActive && playerRb != null)
-                {
-                    playerRb.linearVelocity = Vector3.zero;
-                    playerRb.angularVelocity = Vector3.zero;
-                }
-            }
+            MainActive = !MainActive;
+            MainCamera.gameObject.SetActive(MainActive);
+            TopViewCamera.gameObject.SetActive(!MainActive);
+            Debug.Log("CameraSwitch Pressed");
         }
 
-        if (!mainActive && topRb != null)
+        if (!MainActive) //top view camera movement
         {
-            topInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-            if (Input.GetKeyDown(ResetKey) && ResetPoint != null)
-            {
-                topRb.position = ResetPoint.position;
-                topRb.linearVelocity = Vector3.zero;
-                topRb.angularVelocity = Vector3.zero;
-            }
-        }
-        else topInput = Vector3.zero;
-    }
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-    void FixedUpdate()
-    {
-        if (!mainActive && topRb != null && topInput.sqrMagnitude > 0f)
-            topRb.MovePosition(topRb.position + topInput.normalized * TopViewSpeed * Time.fixedDeltaTime);
+            TopViewCamera.transform.position += move * TopViewSpeed * Time.deltaTime;
+
+            if (Input.GetKeyDown(ResetKey) && ResetPoint)
+                TopViewCamera.transform.position = ResetPoint.position;
+        }
     }
 }
