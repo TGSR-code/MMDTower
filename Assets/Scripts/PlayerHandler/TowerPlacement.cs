4f920ec9c6a3a47d29635ca2b404cbc6c6d86a47
Dean <splinterdeanvalente@gmail.com>
2025-10-28 22:21:07 +0100

    Basic towerplacement finished
    added basic towerplacement with tags and cantplace areas

--- Assets/Scripts/PlayerHandler/TowerPlacement.cs
+++ Assets/Scripts/PlayerHandler/TowerPlacement.cs
@@ -4,29 +4,44 @@ using System.Collections.Generic;
 
 public class TowerPlacement : MonoBehaviour
 {
+    [SerializeField] private LayerMask PlacementCheckMask;
+    [SerializeField] private LayerMask PlacementCollideMask;
     [SerializeField] private Camera PlayerCamera;
-
+    
     private GameObject CurrentPlacingTower;
 
     void Start()
     {
         
     }
-
     
     void Update()
     {
         if (CurrentPlacingTower != null)
         {
             Ray camray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(camray, out RaycastHit hitinfo, 100f))
+            RaycastHit hitinfo;
+
+            if (Physics.Raycast(camray, out hitinfo, 100f, PlacementCollideMask))
             {
                 CurrentPlacingTower.transform.position = hitinfo.point;
             }
 
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0) && hitinfo.collider.gameObject != null)
             {
-                CurrentPlacingTower = null;
+                if(!hitinfo.collider.gameObject.CompareTag("cantPlace"))
+                {
+                    BoxCollider TowerCollider = CurrentPlacingTower.gameObject.GetComponent<BoxCollider>();
+                    TowerCollider.isTrigger = true;
+
+                    Vector3 BoxCenter = CurrentPlacingTower.gameObject.transform.position + TowerCollider.center;
+                    Vector3 HalfExtens = TowerCollider.size / 2;
+                    if (!Physics.CheckBox(BoxCenter, HalfExtens, Quaternion.identity, PlacementCheckMask, QueryTriggerInteraction.Ignore))
+                    {
+                        TowerCollider.isTrigger = false;
+                        CurrentPlacingTower = null;
+                    }
+                }
             }
         }
     }
