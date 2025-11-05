7660b1e0ba3154ed807fba457ef88642ad8cb3bc
Mik982 <mikschenk22@gmail.com>
2025-11-05 08:34:28 +0100

    Test

--- Assets/Scripts/EnemyBehaviour/EnemyPathfinding.cs
+++ Assets/Scripts/EnemyBehaviour/EnemyPathfinding.cs
@@ -65,7 +65,7 @@ public class EnemyPathfinding : waveSystem
 
             }
 
-            if (baseHealth != null && baseHealth.health == 0f)
+            if (baseHealth != null && baseHealth.health <= 0f)
             {print("Died");
                 SceneManager.LoadScene("Gameplay");
             }
